4d5a16762c64290e9cfe1fa8577397d2794e8789
Edie Johnny <edie.eu@gmail.com>
2021-02-01 09:46:19 -0300

    update

--- logdesk/api/consultar_status_internet.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $core = Core::getInstance();
-  $api = $core->loadModule('api');
-  $userid = $api->getUserIDBySession($_SESSION['user']);
-  echo json_encode($api->getInternetStatus($data[0], array('username' => $_SESSION['user'], 'password' => $_SESSION['pass'])));
-  
---
+++ painel/index.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+require 'config.php';
+require 'functions.php';
+
+$_SESSION['token'] = $_POST['token'] ?? false;
+
+spl_autoload_register(function ($class) {
+  if (file_exists('modules/' . $class . '/' . $class . '.php')) {
+    require 'modules/' . $class . '/' . $class . '.php';
+  }
+  if (file_exists('modules/Site/Cadastros/' . $class . '/' . $class . '.php')) {
+    require 'modules/Site/Cadastros/' . $class . '/' . $class . '.php';
+  }
+});
+
+Core::getInstance()->run($config);
