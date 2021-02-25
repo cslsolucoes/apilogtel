c985e744099863b486a68effa4753249771ef6be
Edie Johnny <edie.eu@gmail.com>
2021-02-25 13:26:41 -0300

    qualifica

--- modules/Api/Api.php
+++ modules/Api/Api.php
@@ -327,7 +327,7 @@ class Api {
         $razaoSocial = $array[0]['nome'];
         $nome = $array[0]['nome'];
         $emails = json_decode($array[0]['contato']);
-        print_r($emails);
+        print_r($emails['contato']);
         $contratoStatus = $array[0]['status'];
         $response = array(
           'contratos' => $contratos,
