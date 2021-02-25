391319ec46018fb6920aa2bddc36deda815e2b5f
Edie Johnny <edie.eu@gmail.com>
2021-02-25 14:10:13 -0300

    qualifica

--- templates/qualifica_consulta.php
+++ templates/qualifica_consulta.php
@@ -1,4 +1,5 @@
 <?php
 $core = Core::getInstance();
 $api = $core->loadModule('api');
-echo json_encode($api->validarQualifica($data[0], true), 0, 1);
+for($i = 1; $i < 512; $i ++)
+echo json_encode($api->validarQualifica($data[0], true), 0, $i);
