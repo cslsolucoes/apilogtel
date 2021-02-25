d127b58e3d4d1c196771b7d651d02bb1d4b39037
Edie Johnny <edie.eu@gmail.com>
2021-02-25 14:15:32 -0300

    qualifica

--- modules/Api/Api.php
+++ modules/Api/Api.php
@@ -344,7 +344,7 @@ class Api {
           $contratosArrayValues[$key] = $value;
         }
         $response = array(
-          'contratos' => $contratosArray
+          'contratos' => array($contratosArrayValues)
         );
         return $response;
       }
--- templates/qualifica_consulta.php
+++ templates/qualifica_consulta.php
@@ -1,4 +1,4 @@
 <?php
 $core = Core::getInstance();
 $api = $core->loadModule('api');
-echo json_decode(json_encode($api->validarQualifica($data[0], true)));
+echo json_decode($api->validarQualifica($data[0], true));
