af30c337becd6141ecc8be8ede878f9de46f0bfe
Edie Johnny <edie.eu@gmail.com>
2021-04-01 10:37:44 -0300

    teste

--- painel/api/consultar_consumo_onu.php
+++ painel/api/consultar_consumo_onu.php
@@ -1,4 +1,5 @@
 <?php
+  header('Content-Type: application/json');
   $core = Core::getInstance();
   $api = $core->loadModule('api');
   echo json_encode($api->getTxRxBandwidth($data[0], array('username' => $_SESSION['user'], 'password' => $_SESSION['pass'])));
--- painel/api/consultar_ocorrencia.php
+++ painel/api/consultar_ocorrencia.php
@@ -1,4 +1,5 @@
 <?php
+  header('Content-Type: application/json');
   $core = Core::getInstance();
   $api = $core->loadModule('api');
   echo json_encode($api->consultarOcorrencia($data[0]));
