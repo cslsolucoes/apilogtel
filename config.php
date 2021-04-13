6cd42b7860f8a5ed9d946f38fef2cc0f28939540
Edie Johnny <edie.eu@gmail.com>
2021-04-13 10:26:07 -0300

    config

--- config.php
+++ config.php
@@ -1,29 +1,33 @@
 <?php
-$config = array(
-  'httpHost' => (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]",
-  // DEVELOPMENT
-  // 'sitePath' => 'sistemalogtel/dist/',
-  // PRODUCTION
-  'sitePath' => '/',
-  'assetsFolder' => 'assets',
-  'dbErp' => array(
-    'host' => '201.87.240.202',
-    'port' => 5432,
-    'user' => 'postgres',
-    'pass' => 'postmy',
-    'dbname' => 'dbsgp'
-  ),
-  'dbLocal' => array(
-    'host' => '201.87.240.2',
-    'port' => 5432,
-    'user' => 'postgres',
-    'pass' => '1q2w3e4r5T@',
-    'dbname' => 'logtel'
-  ),
-  'apiSGP' => array(
-    'ip' => '201.87.240.202',
-    'port' => '8000',
-    'app' => 'ura',
-    'token' => '7789c183-98c1-4667-b31f-b20931376f6f'
-  )
-);
+if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']) {
+  $config = array(
+    'httpHost' => (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'],
+    // DEVELOPMENT
+    // 'sitePath' => 'sistemalogtel/dist/',
+    // PRODUCTION
+    'sitePath' => '/',
+    'assetsFolder' => 'assets',
+    'dbErp' => array(
+      'host' => '201.87.240.202',
+      'port' => 5432,
+      'user' => 'postgres',
+      'pass' => 'postmy',
+      'dbname' => 'dbsgp'
+    ),
+    'dbLocal' => array(
+      'host' => '201.87.240.2',
+      'port' => 5432,
+      'user' => 'postgres',
+      'pass' => '1q2w3e4r5T@',
+      'dbname' => 'logtel'
+    ),
+    'apiSGP' => array(
+      'ip' => '201.87.240.202',
+      'port' => '8000',
+      'app' => 'ura',
+      'token' => '7789c183-98c1-4667-b31f-b20931376f6f'
+    )
+  );
+} else {
+  die('invalid request');
+}
--- painel/api/consultar_status_internet.php
+++ painel/api/consultar_status_internet.php
@@ -2,8 +2,9 @@
   header('Content-Type: application/json');
   $core = Core::getInstance();
   $api = $core->loadModule('api');
-  if(!isset($_SESSION['user']) || !isset($_SESSION['pass'])) {
+  if(!isset($_SESSION['user']) || !isset($_SESSION['pass']) || !$_SESSION['user'] || !$_SESSION['pass']) {
     header("Location: " . $uri . "logout");
+    die('invalid session');
   }
   $userid = $api->getUserIDBySession($_SESSION['user']);
   echo json_encode($api->getInternetStatus($data[0], array('username' => $_SESSION['user'], 'password' => $_SESSION['pass'])));
