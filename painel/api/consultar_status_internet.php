364e85dfc225a6d56881a8de058fa987cf50f611
Edie Johnny <edie.eu@gmail.com>
2021-04-14 09:51:25 -0300

    erro status internet

--- painel/api/consultar_status_internet.php
+++ painel/api/consultar_status_internet.php
@@ -3,7 +3,9 @@
   $core = Core::getInstance();
   $api = $core->loadModule('api');
   if(!isset($_SESSION['user']) || !isset($_SESSION['pass']) || !$_SESSION['user'] || !$_SESSION['pass']) {
-    header("Location: " . $uri . "logout");
+    if(isset($uri) && $uri) {
+      header("Location: " . $uri . "logout");
+    }
     die('invalid session');
   }
   $userid = $api->getUserIDBySession($_SESSION['user']);
